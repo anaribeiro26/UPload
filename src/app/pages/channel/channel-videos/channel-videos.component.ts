67e9e5f8875fce622d7be317512a1c513fc041b2
dani <danikvverde@gmail.com>
2022-11-30 12:48:54 +0000

    Test

--- src/app/pages/channel/channel-videos/channel-videos.component.ts
+++ src/app/pages/channel/channel-videos/channel-videos.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChannelVideosComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log('ola')
+  }
 
   ngOnInit(): void {
   }
