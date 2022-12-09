d74d9a25f06508b7246de14bb07212853c2d46dc
Ana Karyne <santacruzanakaryne@gmail.com>
2022-12-09 16:06:04 +0000

    video interface

---
+++ src/app/services/video.model.ts
@@ -0,0 +1,16 @@
+export interface Video {
+  id: string;
+  categorias: string[];
+  tags: string[];
+  descricao: string;
+  data: string;
+  video: string;
+  titulo: string;
+  categorias_id: string;
+  tags_id: string;
+  canal: string;
+  canal_id: string;
+
+}
+
+
