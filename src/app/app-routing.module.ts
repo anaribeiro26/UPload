01658a28f612c7898b5fe9ad88224255477fae7c
irinaborges <irinaborges@hotmail.com>
2022-12-15 19:23:25 +0000

    Channel routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,13 @@ import {FavouritesComponent} from "./pages/favourites/favourites.component";
 import {HomePageComponent} from "./pages/home-page/home-page.component";
 import {PlaylistsComponent} from "./pages/playlists/playlists.component";
 import {VideoDetailsComponent} from "./pages/home-page/video-details/video-details.component";
+import {ChannelsListComponent} from "./pages/channel/channels-list/channels-list.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/homepage', pathMatch: 'full'},
   {path: 'homepage', component: HomePageComponent},
   {path: 'video/:id', component: VideoDetailsComponent},
-  {path: 'channels', component: ChannelComponent},
+  {path: 'channels', component: ChannelsListComponent},
   {path: 'channels/:id', component: ChannelComponent},
   {path: 'favourites', component: FavouritesComponent},
   {path: 'playlists', component: PlaylistsComponent},
