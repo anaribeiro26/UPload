c4359ab5fb98d5aafa6085f469fc6561e047522a
irinaborges <irinaborges@hotmail.com>
2022-12-19 15:48:32 +0000

    Tags page

---
+++ src/app/pages/articles/tags/tags.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import {Tags} from "../../../services/UPload.model";
+import {ActivatedRoute} from "@angular/router";
+import {UPloadService} from "../../../services/UPload.service";
+
+@Component({
+  selector: 'app-tags',
+  templateUrl: './tags.component.html',
+  styleUrls: ['./tags.component.scss']
+})
+export class TagsComponent implements OnInit {
+
+  tags: Tags[] = [];
+
+  constructor(private route: ActivatedRoute, private UPload: UPloadService) {
+  }
+
+  ngOnInit(): void {
+    let id = this.route.snapshot.params['id'];
+    this.UPload.getTags(id).subscribe((tags) => {
+      this.tags = tags as Tags[];
+    })
+  }
+}
