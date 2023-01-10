29fb36e78b734c95d52e8f8e5f13e8a54d8cc154
irinaborges <irinaborges@hotmail.com>
2023-01-10 15:55:41 +0000

    Tags adjusted

--- src/app/pages/channel/channel-videos/channel-videos.component.ts
+++ src/app/pages/channel/channel-videos/channel-videos.component.ts
@@ -11,16 +11,24 @@ export class ChannelVideosComponent implements OnInit {
 
   videos: ChannelVideos[] = [];
 
-  image_url = '/hqdefault.jpg'
+  image_url = '/hqdefault.jpg';
+
   @Input() channel_id!: number;
 
+
   constructor(private UPload: UPloadService) {
+
   }
 
   ngOnInit(): void {
     this.UPload.getChannelVideos(this.channel_id).subscribe((videos) => {
       this.videos = videos as ChannelVideos[];
+      this.videos.forEach(video => {
+        let word = video.date.split(" ");
+        if(word.length > 2) {
+          video.date = `${word[0]} ${word[1]} e ${word[2]} ${word[3]}`
+        }
+      })
     })
   }
-
 }
