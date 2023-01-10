a6b34f95997a5943873914c5eeb2e41d1743de39
irinaborges <irinaborges@hotmail.com>
2023-01-10 15:55:14 +0000

    Channel comments

--- src/app/pages/channel/channel-comments/channel-comments.component.ts
+++ src/app/pages/channel/channel-comments/channel-comments.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ChannelComments} from "../../../services/UPload.model";
+import {UPloadService} from "../../../services/UPload.service";
 
 @Component({
   selector: 'app-channel-comments',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./channel-comments.component.scss']
 })
 export class ChannelCommentsComponent implements OnInit {
+  comments: ChannelComments[] = [];
+  @Input() channel_id!: number;
 
-  constructor() { }
+  constructor(private UPload: UPloadService) {
+  }
 
   ngOnInit(): void {
+    this.UPload.getChannelComments(this.channel_id).subscribe((comments) => {
+      this.comments = comments as ChannelComments[];
+    })
   }
 
 }
+
