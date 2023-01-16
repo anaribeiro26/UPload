770e8bd8006741ac433ce02854069f769471f169
irinaborges <irinaborges@hotmail.com>
2023-01-16 18:37:12 +0000

    Adjusted mobile CSS

--- src/app/pages/channel/list/channels-list.component.ts
+++ src/app/pages/channel/list/channels-list.component.ts
@@ -28,18 +28,20 @@ export class ChannelsListComponent implements OnInit {
     this.UPload.getChannels().subscribe((channelsList) => {
       this.channelsList = channelsList as Channels[];
     })
-  function channelSearch() {
-    //let input = this.searchbar.value.toLowerCase()
-    let input = (<HTMLInputElement>document.getElementById("searchbar")).value.toLowerCase()
-    let x = document.querySelectorAll('content');
-    for (let i = 0; i < x.length; i++) {
-      if (!x[i].innerHTML.toLowerCase().includes(input)) {
-        x[i].setAttribute("style", "display: none;");
-      } else {
-        x[i].setAttribute("style", "display: list-item;");
-      }
-    }
-  }
+
+ //function channelSearch() {
+ //  //let input = this.searchbar.value.toLowerCase()
+ //  let input = (<HTMLInputElement>document.getElementById("searchbar")).value.toLowerCase()
+ //  let x = document.querySelectorAll('content');
+ //  for (let i = 0; i < x.length; i++) {
+ //    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+ //      x[i].setAttribute("style", "display: none;");
+ //    } else {
+ //      x[i].setAttribute("style", "display: list-item;");
+ //    }
+ //  }
+ //}
+
    //function channelSearch() {
    //  let input = (<HTMLInputElement>document.getElementById('searchbar')).value.toLowerCase()
    //  let x = document.getElementsByClassName('content');
