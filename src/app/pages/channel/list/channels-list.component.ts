886ac9a6ef7dfb50eef35346f190f02a44463e4f
irinaborges <irinaborges@hotmail.com>
2023-01-17 13:14:26 +0000

    CSS adjusted

--- src/app/pages/channel/list/channels-list.component.ts
+++ src/app/pages/channel/list/channels-list.component.ts
@@ -5,18 +5,6 @@ import {ElementRef, ViewChild} from "@angular/core";
 import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
 import {Channels} from "../../../services/UPload.model";
 import {FilterPipe} from "../../../pipe/filter.pipe";
-export function channelSearch() {
-  //let input = this.searchbar.value.toLowerCase()
-  let input = (<HTMLInputElement>document.getElementById("searchbar")).value.toLowerCase()
-  let x = document.querySelectorAll('content');
-  for (let i = 0; i < x.length; i++) {
-    if (!x[i].innerHTML.toLowerCase().includes(input)) {
-      x[i].setAttribute("style", "display: none;");
-    } else {
-      x[i].setAttribute("style", "display: list-item;");
-    }
-  }
-}
 
 @Component({
   selector: 'app-channel-list',
@@ -28,10 +16,11 @@ export class ChannelsListComponent implements OnInit {
   faMagnifyingGlass = faMagnifyingGlass;
   channelsList: Channels[] = [];
   searchbar = '';
-  FilterPipe = FilterPipe;
+  // FilterPipe = FilterPipe;
   //searchbar: string;
   image_url = "https://dev-project-upskill-grupo05.pantheonsite.io";
- // @ViewChild('searchbar', { static: true }) public searchbar: ElementRef;
+
+  // @ViewChild('searchbar', { static: true }) public searchbar: ElementRef;
 
   constructor(private route: ActivatedRoute, private UPload: UPloadService, private elem: ElementRef) {
   }
@@ -40,6 +29,19 @@ export class ChannelsListComponent implements OnInit {
     this.UPload.getChannels().subscribe((channelsList) => {
       this.channelsList = channelsList as Channels[];
     })
+
+    function channelSearch() {
+      //let input = this.searchbar.value.toLowerCase()
+      let input = (<HTMLInputElement>document.getElementById("searchbar")).value.toLowerCase()
+      let x = document.querySelectorAll('content');
+      for (let i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+          x[i].setAttribute("style", "display: none;");
+        } else {
+          x[i].setAttribute("style", "display: list-item;");
+        }
+      }
+    }
 
 //function channelSearch() {
 //  //let input = this.searchbar.value.toLowerCase()
@@ -54,36 +56,36 @@ export class ChannelsListComponent implements OnInit {
 //  }
 //}
 
-   //function channelSearch() {
-   //  let input = (<HTMLInputElement>document.getElementById('searchbar')).value.toLowerCase()
-   //  let x = document.getElementsByClassName('content');
+    //function channelSearch() {
+    //  let input = (<HTMLInputElement>document.getElementById('searchbar')).value.toLowerCase()
+    //  let x = document.getElementsByClassName('content');
 
-   //  for (let i = 0; i < x.length; i++) {
-   //    if (!x[i].innerHTML.toLowerCase().includes(input)) {
-   //      x[i].setAttribute("style", "display: none;");
-   //    } else {
-   //      x[i].setAttribute("style", "display: list-item;");
-   //    }
-   //  }
-   //}
-   //let onKeyUp = (x: any) => {
-   //  this.text += x.target.value + ' | ';
-   //}
-   //   function channelSearch() {
-   //   let input = (<HTMLInputElement>document.getElementById('searchbar')).value.toLowerCase()
-   //   // let input = (<HTMLInputElement>document.getElementById('searchbar')).value.toLowerCase()
-   //   //input = input.toLowerCase();
-   //   let x = document.getElementsByClassName('content');
+    //  for (let i = 0; i < x.length; i++) {
+    //    if (!x[i].innerHTML.toLowerCase().includes(input)) {
+    //      x[i].setAttribute("style", "display: none;");
+    //    } else {
+    //      x[i].setAttribute("style", "display: list-item;");
+    //    }
+    //  }
+    //}
+    //let onKeyUp = (x: any) => {
+    //  this.text += x.target.value + ' | ';
+    //}
+    //   function channelSearch() {
+    //   let input = (<HTMLInputElement>document.getElementById('searchbar')).value.toLowerCase()
+    //   // let input = (<HTMLInputElement>document.getElementById('searchbar')).value.toLowerCase()
+    //   //input = input.toLowerCase();
+    //   let x = document.getElementsByClassName('content');
 //
-   //   for (let i = 0; i < x.length; i++) {
-   //     if (!x[i].innerHTML.toLowerCase().includes(input)) {
-   //       x[i].setAttribute("style", "display: none;");
-   //       // x[i].style.display="none";
-   //     } else {
-   //       x[i].setAttribute("style", "display: list-item;");
-   //       //  x[i].style.display="list-item";
-   //     }
-   //   }
-   // }
+    //   for (let i = 0; i < x.length; i++) {
+    //     if (!x[i].innerHTML.toLowerCase().includes(input)) {
+    //       x[i].setAttribute("style", "display: none;");
+    //       // x[i].style.display="none";
+    //     } else {
+    //       x[i].setAttribute("style", "display: list-item;");
+    //       //  x[i].style.display="list-item";
+    //     }
+    //   }
+    // }
   }
 }
