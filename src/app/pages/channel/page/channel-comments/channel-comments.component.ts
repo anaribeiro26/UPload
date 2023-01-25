909c82dfef41d7fca31e9b4e8a1d4883faf40dcc
dani <danikvverde@gmail.com>
2023-01-25 20:04:43 +0000

    validação canal

--- src/app/pages/channel/page/channel-comments/channel-comments.component.ts
+++ src/app/pages/channel/page/channel-comments/channel-comments.component.ts
@@ -1,8 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ChannelComments} from "../../../../services/UPload.model";
 import {UPloadService} from "../../../../services/UPload.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import {NgForm} from "@angular/forms";
-import {faFlag} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-channel-comments',
@@ -10,17 +10,30 @@ import {faFlag} from "@fortawesome/free-solid-svg-icons";
   styleUrls: ['./channel-comments.component.scss']
 })
 export class ChannelCommentsComponent implements OnInit {
+  meuFormGroup: FormGroup;
   comments: ChannelComments[] = [];
-  lang = localStorage.getItem('lang') || 'pt'
   title = "Comentários"
   url = "https://robohash.org/";
   set = "?set=set4&bgset=bg2"
   random = (Math.random() + 1).toString(36).substring(7);
   @Input() channel_id!: number;
-  faFlag = faFlag;
 
-
-  constructor(private UPload: UPloadService) {
+  constructor(private UPload: UPloadService, private formBuilder: FormBuilder) {
+    this.meuFormGroup = this.formBuilder.group({
+      name: ['', [
+        Validators.required,
+        Validators.minLength(2)]],
+      comment: ['', [
+        Validators.required,
+        Validators.minLength(2)]],
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      id: "0",
+      channel_id:this.channel_id,
+      date: "0 segundos"
+    });
   }
 
   ngOnInit(): void {
@@ -30,20 +43,20 @@ export class ChannelCommentsComponent implements OnInit {
     })
   }
 
-  handleCommentFormSubmit(data: { name: string; email: string; message: string }, userPost: NgForm){
+  handleCommentFormSubmit(data: { name: string; email: string; comment: string }, userPost: any){
 
     console.log(data)
     const nameInput = data.name.valueOf()
     console.log(nameInput)
     const emailInput = data.email.valueOf()
-    const messageInput = data.message.valueOf()
+    const messageInput = data.comment.valueOf()
 
 
     if (this.reloadPage(nameInput, emailInput, messageInput)) {
-      this.UPload.commentChannel(this.channel_id, data.name, data.email, data.message).subscribe((response) => {
+      this.UPload.commentChannel(this.channel_id, data.name, data.email, data.comment).subscribe((response) => {
         this.comments.unshift({
           name: data.name,
-          comment: data.message,
+          comment: data.comment,
           id: parseInt("0"),
           channel_id: this.channel_id,
           date: "0 segundos"
@@ -55,27 +68,13 @@ export class ChannelCommentsComponent implements OnInit {
     }
   }
 
-  handleReport(id: number) {
-    this.UPload.reportChannelComment(id).subscribe();
-    console.log(id)
-
-    let toast: any = document.getElementById("snackbar");
-    toast.className = "show";
-    setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 3000);
-  }
-
   // @ts-ignore
   reloadPage(nameInput, emailInput, messageInput) {
-    if (nameInput.length > 2 && emailInput.length > 2 && messageInput.length > 2 ) {
-      return true
-    } else {
-      return false
-    }
+    return  nameInput.length > 2 && emailInput.length > 2 && messageInput.length > 2
   }
 
   errorMessage() {
     alert('erro')
   }
-
 }
 
