28d88b08c673c9e8d7169c228e881f81ce5e0f8c
irinaborges <irinaborges@hotmail.com>
2023-01-25 16:11:02 +0000

    Added report comment translation

--- src/app/pages/video-details/video-comments/video-comments.component.ts
+++ src/app/pages/video-details/video-comments/video-comments.component.ts
@@ -14,6 +14,7 @@ import {faFlag} from "@fortawesome/free-solid-svg-icons";
 export class VideoCommentsComponent implements OnInit {
   meuFormGroup: FormGroup;
   videoComments: VideoComments[] = [];
+  lang = localStorage.getItem('lang') || 'pt'
   title = "Comentários"
   url = "https://robohash.org/";
   set = "?set=set4&bgset=bg2";
