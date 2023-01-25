a53d1134e5730129eb7a6d8e344cce301ad59a66
irinaborges <irinaborges@hotmail.com>
2023-01-25 15:47:41 +0000

    Category videos added

---
+++ src/app/pages/categories/category-videos/category-videos.component.ts
@@ -0,0 +1,35 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {TaxonomyVideos} from "../../../services/UPload.model";
+import {UPloadService} from "../../../services/UPload.service";
+import {TranslateService} from "@ngx-translate/core";
+
+@Component({
+  selector: 'app-category-videos',
+  templateUrl: './category-videos.component.html',
+  styleUrls: ['./category-videos.component.scss']
+})
+export class CategoryVideosComponent implements OnInit {
+  category_videos: TaxonomyVideos[] = [];
+  and:any;
+  @Input() category_id!: number;
+  image_url = '/hqdefault.jpg'
+
+  constructor(private UPload: UPloadService, private translate: TranslateService) {
+  }
+
+  ngOnInit(): void {
+    this.UPload.getCategoryVideos(this.category_id).subscribe((category_videos) => {
+      this.category_videos = category_videos as TaxonomyVideos[];
+      this.translate.get('upload.and').subscribe(and => {
+        this.and = (and);
+      });
+      this.category_videos.forEach(video => {
+        let word = video.date.split(" ");
+        if(word.length > 2) {
+          video.date = `${word[0]} ${word[1]} ${this.and} ${word[2]} ${word[3]}`
+        }
+      })
+    })
+  }
+}
+
