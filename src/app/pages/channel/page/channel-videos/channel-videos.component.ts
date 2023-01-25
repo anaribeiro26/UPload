08f0f32f8195bdaccda38f4721e0a6d488803a09
irinaborges <irinaborges@hotmail.com>
2023-01-25 02:59:44 +0000

    Added action buttons to channel videos

--- src/app/pages/channel/page/channel-videos/channel-videos.component.ts
+++ src/app/pages/channel/page/channel-videos/channel-videos.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, OnInit} from '@angular/core';
 import {UPloadService} from "../../../../services/UPload.service";
 import {TranslateService} from "@ngx-translate/core";
 import {ChannelVideos} from "../../../../services/UPload.model";
+import {faBookmark} from "@fortawesome/free-regular-svg-icons";
+import {faBookmark as faBookmarkSolid} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-channel-videos',
@@ -11,8 +13,10 @@ import {ChannelVideos} from "../../../../services/UPload.model";
 export class ChannelVideosComponent implements OnInit {
 
   videos: ChannelVideos[] = [];
-  and:any;
+  and: any;
   lang = localStorage.getItem('lang') || 'pt'
+  faBookmarkSolid = faBookmarkSolid;
+  faBookmark = faBookmark;
   image_url = '/hqdefault.jpg';
   @Input() channel_id!: number;
 
@@ -28,10 +32,36 @@ export class ChannelVideosComponent implements OnInit {
       });
       this.videos.forEach(video => {
         let word = video.date.split(" ");
-        if(word.length > 2) {
+        if (word.length > 2) {
           video.date = `${word[0]} ${word[1]} ${this.and} ${word[2]} ${word[3]}`
         }
       })
     })
+  }
+
+  changeFavorite(id: number) {
+    this.UPload.toggleFavorite(id)
+  }
+
+  favourite(id: number) {
+    return this.UPload.isFavorite(id)
+  }
+
+  share(txt: string) {
+    console.log(txt)
+  }
+
+  clickShare(myUrl: string) {
+    if (navigator.share) {
+      navigator.share({
+        title: 'Copied link here.',
+        url: myUrl
+      }).then(() => {
+        console.log('Thanks for sharing!');
+      })
+        .catch(error => console.log('Error sharing', error));
+    } else {
+      alert('Share not supported!');
+    }
   }
 }
