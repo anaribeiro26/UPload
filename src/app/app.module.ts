a98d4822e943af5070036ed9e1b5e39f80476d6c
irinaborges <irinaborges@hotmail.com>
2023-01-26 14:23:06 +0000

    Corrected pipe name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,17 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {VideoDetailsComponent} from './pages/video-details/video-details.component';
 import {VideoCommentsComponent} from './pages/video-details/video-comments/video-comments.component';
-import {ChannelCommentsComponent} from './pages/channel/details/channel-comments/channel-comments.component';
-import {ChannelComponent} from './pages/channel/details/channel.component';
+import {ChannelCommentsComponent} from './pages/channels/details/channel-comments/channel-comments.component';
+import {ChannelComponent} from './pages/channels/details/channel.component';
 import {HomepageComponent} from './pages/homepage/homepage.component';
 import {PlaylistsComponent} from './pages/playlists/playlists.component';
 import {FavouritesComponent} from "./pages/favourites/favourites.component";
 import {ThematicsComponent} from './pages/thematics/thematics.component';
 import {NavigationComponent} from './components/navigation/navigation.component';
-import {ChannelVideosComponent} from './pages/channel/details/channel-videos/channel-videos.component';
+import {ChannelVideosComponent} from './pages/channels/details/channel-videos/channel-videos.component';
 import {HttpClientModule, HttpClient} from "@angular/common/http";
 import {PlaylistDetailsComponent} from './pages/playlists/playlist-details/playlist-details.component';
-import {ChannelsListComponent} from './pages/channel/list/channels-list.component';
+import {ChannelsListComponent} from './pages/channels/list/channels-list.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {NgxYoutubePlayerModule} from 'ngx-youtube-player';
 import {TagsComponent} from './pages/tags/tags.component';
@@ -23,12 +23,12 @@ import {TagVideosComponent} from './pages/tags/tag-videos/tag-videos.component';
 import {PlaylistVideosComponent} from './pages/playlists/playlist-videos/playlist-videos.component';
 import {SuggestedVideosComponent} from "./pages/video-details/suggested-videos/suggested-videos.component";
 import {SuggestedThematicsComponent} from "./pages/homepage/suggested-thematics/suggested-thematics.component";
-import {FavouritesVideosComponent} from './pages/favourites/favourites-details/favourites-videos.component';
+import {FavouriteVideosComponent} from './pages/favourites/favourite-videos/favourite-videos.component';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {ThematicDetailsComponent} from './pages/thematics/details/thematic-details.component';
 import {ThematicVideosComponent} from './pages/thematics/details/thematic-videos/thematic-videos.component';
-import {FilterPipe} from './pipe/filter.pipe';
+import {replaceAllPipe} from './pipe/replaceAll.pipe';
 import {YoutubePlayerComponent} from "./pages/video-details/youtube-player/youtube-player.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SuggestedChannelsComponent} from './pages/homepage/suggested-channels/suggested-channels.component';
@@ -55,10 +55,10 @@ import {CategoryVideosComponent} from './pages/categories/category-videos/catego
     PlaylistVideosComponent,
     SuggestedVideosComponent,
     SuggestedThematicsComponent,
-    FavouritesVideosComponent,
+    FavouriteVideosComponent,
     ThematicDetailsComponent,
     ThematicVideosComponent,
-    FilterPipe,
+    replaceAllPipe,
     YoutubePlayerComponent,
     SuggestedChannelsComponent,
     CategoriesComponent,
