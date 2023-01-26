1aca1480d9823e3feafd72991aef6d26aa1aa2bb
irinaborges <irinaborges@hotmail.com>
2023-01-26 14:24:46 +0000

    Corrected paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ThematicsComponent} from "./pages/thematics/thematics.component";
-import {ChannelComponent} from "./pages/channel/details/channel.component";
+import {ChannelComponent} from "./pages/channels/details/channel.component";
 import {HomepageComponent} from "./pages/homepage/homepage.component";
 import {PlaylistsComponent} from "./pages/playlists/playlists.component";
 import {VideoDetailsComponent} from "./pages/video-details/video-details.component";
-import {ChannelsListComponent} from "./pages/channel/list/channels-list.component";
+import {ChannelsListComponent} from "./pages/channels/list/channels-list.component";
 import {TagsComponent} from "./pages/tags/tags.component";
 import {PlaylistDetailsComponent} from "./pages/playlists/playlist-details/playlist-details.component";
-import {FavouritesVideosComponent} from "./pages/favourites/favourites-details/favourites-videos.component";
+import {FavouriteVideosComponent} from "./pages/favourites/favourite-videos/favourite-videos.component";
 import {ThematicDetailsComponent} from "./pages/thematics/details/thematic-details.component";
 import {CategoriesComponent} from "./pages/categories/categories.component";
 
@@ -17,10 +17,10 @@ const routes: Routes = [
   {path: 'thematics', component: ThematicsComponent},
   {path: 'thematic/:title', component: ThematicDetailsComponent},
   {path: 'channels', component: ChannelsListComponent},
-  {path: 'channel/:title', component: ChannelComponent},
+  {path: 'channels/:title', component: ChannelComponent},
   {path: 'homepage', component: HomepageComponent},
   {path: 'video/:title', component: VideoDetailsComponent},
-  {path: 'playlist/favourites', component: FavouritesVideosComponent},
+  {path: 'playlist/favourites', component: FavouriteVideosComponent},
   {path: 'playlists', component: PlaylistsComponent},
   {path: 'playlist/:title', component: PlaylistDetailsComponent},
   {path: 'tags/:name', component: TagsComponent},
