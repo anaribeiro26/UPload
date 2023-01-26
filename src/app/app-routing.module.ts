65d9842c1c56a0fbc897401259e65e7e210bd5d8
irinaborges <irinaborges@hotmail.com>
2023-01-26 14:08:51 +0000

    Correcting paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {ThematicsComponent} from "./pages/thematics/list/thematics.component";
+import {ThematicsComponent} from "./pages/thematics/thematics.component";
 import {ChannelComponent} from "./pages/channel/details/channel.component";
-import {FavouritesComponent} from "./pages/favourites/favourites.component";
-import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {HomepageComponent} from "./pages/homepage/homepage.component";
 import {PlaylistsComponent} from "./pages/playlists/playlists.component";
 import {VideoDetailsComponent} from "./pages/video-details/video-details.component";
 import {ChannelsListComponent} from "./pages/channel/list/channels-list.component";
 import {TagsComponent} from "./pages/tags/tags.component";
 import {PlaylistDetailsComponent} from "./pages/playlists/playlist-details/playlist-details.component";
-import {FavouritesVideosComponent} from "./pages/favourites/favourites-videos/favourites-videos.component";
-import {ThematicDetailsComponent} from "./pages/thematics/page/thematic-details/thematic-details.component";
+import {FavouritesVideosComponent} from "./pages/favourites/favourites-details/favourites-videos.component";
+import {ThematicDetailsComponent} from "./pages/thematics/details/thematic-details.component";
 import {CategoriesComponent} from "./pages/categories/categories.component";
 
 const routes: Routes = [
@@ -19,8 +18,8 @@ const routes: Routes = [
   {path: 'thematic/:title', component: ThematicDetailsComponent},
   {path: 'channels', component: ChannelsListComponent},
   {path: 'channel/:title', component: ChannelComponent},
-  {path: 'homepage', component: HomePageComponent},
-  {path: 'video/:id', component: VideoDetailsComponent},
+  {path: 'homepage', component: HomepageComponent},
+  {path: 'video/:title', component: VideoDetailsComponent},
   {path: 'playlist/favourites', component: FavouritesVideosComponent},
   {path: 'playlists', component: PlaylistsComponent},
   {path: 'playlist/:title', component: PlaylistDetailsComponent},
